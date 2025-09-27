0be2d75b49d687f0c3039187ce310c4548e9e605
Annie <annmora21@gmail.com>
2025-09-27 12:15:50 -0700

    v-0.1

---
+++ l-15.cpp
@@ -0,0 +1,24 @@
+// COMSC-210 | Lab 15 | Annie Morales
+// IDE used: Visual Studio Code
+
+#include <string>
+using namespace std;
+
+class Movie
+{
+private:
+	string screenwriter;
+    int yearReleased;
+    string title;
+
+public:
+// Getters and Setters
+string getScreenwriter()        { return screenwriter; }
+void setSW(string sw)           { screenwriter = sw; }
+}
+
+int main()
+{
+	// code here
+	return 0;
+}
