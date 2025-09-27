df624b5380828518f4d15d1f30a3438bab6c2fcd
Annie <annmora21@gmail.com>
2025-09-27 12:26:24 -0700

    v-0.2

--- l-15.cpp
+++ l-15.cpp
@@ -1,8 +1,12 @@
 // COMSC-210 | Lab 15 | Annie Morales
 // IDE used: Visual Studio Code
 
+#include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
+
+const int W15 = 15;
 
 class Movie
 {
@@ -15,7 +19,23 @@ public:
 // Getters and Setters
 string getScreenwriter()        { return screenwriter; }
 void setSW(string sw)           { screenwriter = sw; }
+
+int getYearReleased()           { return yearReleased; }
+void setYrReleased(int YrR)     { yearReleased = YrR; }
+
+string getTitle()               { return title; }
+void setTitle(string t)         { title = t;}
+
+// Print method
+void print()
+{
+    cout << setw(W15) << "Screenwriter: " << screenwriter << endl;
+    cout << setw(W15) << "Year released: " << yearReleased << endl;
+    cout << setw(W15) << "Title: " << title << endl;
 }
+};
+
+void print(Movie);
 
 int main()
 {
